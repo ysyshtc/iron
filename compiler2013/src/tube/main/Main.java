52c416a6738a678a5e50ceea54f476af1a7351ab
ysyshtc <ysyshtc@60f7c8e4-da04-4ef6-9c4e-7168c5b70872>
2013-04-12 09:46:08 +0000

    hello
    git-svn-id: https://202.120.61.16/svn/ysyshtc@10 60f7c8e4-da04-4ef6-9c4e-7168c5b70872

---
+++ compiler2013/src/tube/main/Main.java
@@ -0,0 +1,47 @@
+package tube.main;
+
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
+import tube.ast.Program;
+import tube.syntactic.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		for (String fileName : args)
+			new Main().compile(fileName);
+	}
+	void compile(String fileName) throws IOException {
+		InputStream source = new FileInputStream(fileName);
+		
+		ANTLRInputStream stream = new ANTLRInputStream(source);
+		GrammarLexer lex = new GrammarLexer(stream);
+		CommonTokenStream tokens = new CommonTokenStream(lex);
+		tokens.fill();
+		List<Token> list = tokens.getTokens();
+
+		PrintWriter output = new PrintWriter(new FileOutputStream(fileName.replace(".c", ".lexer")));
+		for (Token iter : list) {
+			if (iter.getType() != -1)
+				output.println(lex.tokenNames[iter.getType()] + ", " + iter.getText());
+		}
+		output.close();
+
+		GrammarParser parser = new GrammarParser(tokens);
+
+		output = new PrintWriter(new FileOutputStream(fileName.replace(".c", ".pt")));
+		ParseTree tree = parser.program();
+		output.println(tree.toStringTree(parser));
+		output.close();
+
+		output = new PrintWriter(new FileOutputStream(fileName.replace(".c", ".ast")));
+		Program prog = new Program(parser);
+		output.println(prog);
+		output.close();
+	}
+}
+
