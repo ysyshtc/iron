4f5c42746767b59eac1e8b26021c984ce5e14236
ysyshtc <ysyshtc@60f7c8e4-da04-4ef6-9c4e-7168c5b70872>
2013-04-12 14:53:28 +0000

    hello
    git-svn-id: https://202.120.61.16/svn/ysyshtc@16 60f7c8e4-da04-4ef6-9c4e-7168c5b70872

---
+++ Main.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class Main {
+	public static void main(String[] args) {
+		new Main().run();
+	}
+	void run() {
+		InputReader in = new InputReader(System.in);
+		PrintWriter out = new PrintWriter(System.out);
+
+		out.close();
+	}
+}
+
+class InputReader {
+	BufferedReader buff;
+	StringTokenizer tokenizer;
+
+	InputReader(InputStream stream) {
+		buff = new BufferedReader(new InputStreamReader(stream));
+		tokenizer = null;
+	}
+	boolean hasNext() {
+		while (tokenizer == null || !tokenizer.hasMoreTokens())
+			try {
+				tokenizer = new StringTokenizer(buff.readLine());
+			}
+			catch (Exception e) {
+				return false;
+			}
+		return true;
+	}
+	String next() {
+		if (!hasNext())
+			throw new RuntimeException();
+		return tokenizer.nextToken();
+	}
+	int nextInt() { return Integer.parseInt(next()); }
+	long nextLong() { return Long.parseLong(next()); }
+}
+
